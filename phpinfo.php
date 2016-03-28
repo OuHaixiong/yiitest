42e0d9630b0835de57f75f64a01a108e4ce6aac0
OuHaixiong <258333309@163.com>
2016-03-28 15:56:00 +0800

    测试提交

--- phpinfo.php
+++ phpinfo.php
@@ -1,16 +1,2 @@
 <?php
-//phpinfo();
-// attach
-
-$data = array(
-//     '', 
-    'memberId' => 'dcfab3e4-8d72-4795-a64c-2c51e3664a99',
-    'ip' => '123.456.789.683',
-    'module' => 'wslm',
-    'payWay' => 'wxpay',
-    'action' => 'pay',
-);
-$json = json_encode($data);
-var_dump($json);
-
-// {memberId:'',ip:'',balance:'0',action:'pay',module:'wslm',payWay:'wxpay',shirdShowId:''}
+phpinfo();
